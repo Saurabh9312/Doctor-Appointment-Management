5ce823d9eb5dc2d60c0077e1a7ab7ad6b06ea723
Saurabh9312 <saurabhparate054@gmail.com>
2025-12-28 19:59:14 +0530

    Updated view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('doctorAppointment.urls')),
+    path('', home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
